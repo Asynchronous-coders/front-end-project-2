1978a83c07a1d8b01cea454696ac3bdccf323327
Juan Osorio <juan.michael.osorio@gmail.com>
2020-07-14 21:38:25 -0500

    set the calls to the backend and added wine icons in css

--- assets/index.js
+++ assets/index.js
@@ -1,103 +1,173 @@
-$(document).ready(function (){
+
+
+$(document).ready(function () {
   // List Filters EndPoint
   listFilters();
-  function listFilters(){
+  function listFilters() {
     $.ajax({
-        "async": true,
-        "crossDomain": true,
-        "url": "https://the-cocktail-db.p.rapidapi.com/list.php?a=list",
-        "method": "GET",
-        "headers": {
-          "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
-          "x-rapidapi-key": "800dedb80dmsh5623edb79c19968p10818fjsnd60acfd537d3"
-        }
-        
-    }).then(function(response){
-      for(i = 0; i < 3; i ++){
-        $('.filterOption').append("<div>" + response.drinks[i].strAlcoholic + "</div>" + "<br>"); 
+      "async": true,
+      "crossDomain": true,
+      "url": "https://the-cocktail-db.p.rapidapi.com/list.php?a=list",
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
+        "x-rapidapi-key": "800dedb80dmsh5623edb79c19968p10818fjsnd60acfd537d3"
+      }
+
+    }).then(function (response) {
+      for (i = 0; i < 3; i++) {
+        $('.filterOption').append("<div>" + response.drinks[i].strAlcoholic + "</div>" + "<br>");
       }
     });
-
   };
   // End Filters Endpoint
 
   // List Ingredients
   listIngredients();
-    function listIngredients() {
-        $.ajax({
-            async: true,
-            url: "https://the-cocktail-db.p.rapidapi.com/list.php?i=list",
-            method: "GET",
-            headers : {	
-            "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
-            "x-rapidapi-key": "800dedb80dmsh5623edb79c19968p10818fjsnd60acfd537d3",
-          }
-        }).then(function (response) {
-          for(i = 0; i <= 100; i ++){
-            $('.ingredientsOption').append("<div>" + response.drinks[i].strIngredient1  + "</div>" + "<br>"); 
-          }
-        });
-        // End Ingredient List
+  function listIngredients() {
+    $.ajax({
+      async: true,
+      url: "https://the-cocktail-db.p.rapidapi.com/list.php?i=list",
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
+        "x-rapidapi-key": "800dedb80dmsh5623edb79c19968p10818fjsnd60acfd537d3",
+      }
+    }).then(function (response) {
+      for (i = 0; i <= 100; i++) {
+        $('.ingredientsOption').append("<div>" + response.drinks[i].strIngredient1 + "</div>" + "<br>");
+      }
+    });
+  };
+    // End Ingredient List
 
-        // List Glasses List
-        listGlasses();
-        function listGlasses() {
-            $.ajax({
-                async: true,
-                url: "https://the-cocktail-db.p.rapidapi.com/list.php?g=list",
-                method: "GET",
-                headers : {	
-                "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
-                "x-rapidapi-key": "800dedb80dmsh5623edb79c19968p10818fjsnd60acfd537d3",
-              }
-            }).then(function (response) {
-              for(i = 0; i <= 32; i ++){
-                $('.glassesOption').append("<div>" + response.drinks[i].strGlass  + "</div>" + "<br>"); 
-              }
-            });
-        // End Glasses List
+    // List Glasses List
+    listGlasses();
+    function listGlasses() {
+      $.ajax({
+        async: true,
+        url: "https://the-cocktail-db.p.rapidapi.com/list.php?g=list",
+        method: "GET",
+        headers: {
+          "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
+          "x-rapidapi-key": "800dedb80dmsh5623edb79c19968p10818fjsnd60acfd537d3",
+        }
+      }).then(function (response) {
+        for (i = 0; i <= 32; i++) {
+          $('.glassesOption').append("<div>" + response.drinks[i].strGlass + "</div>" + "<br>");
+        }
+      });
+    };
+    // End Glasses List
 
-        // List Glasses List
-listCategories();
-      function listCategories() {
-          $.ajax({
-              async: true,
-              url: "https://the-cocktail-db.p.rapidapi.com/list.php?c=list",
-              method: "GET",
-              headers : {	
-              "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
-              "x-rapidapi-key": "800dedb80dmsh5623edb79c19968p10818fjsnd60acfd537d3",
-            }
-          }).then(function (response) {
-            for(i = 0; i <= 32; i ++){
-              $('.categoriesOption').append("<div>" + response.drinks[i].strCategory + "</div>" + "<br>"); 
-            }
-          });
-      // End Glasses List
-  // Search bar / find drink by name
- 
+    // List Glasses List
+    listCategories();
+    function listCategories() {
+      $.ajax({
+        async: true,
+        url: "https://the-cocktail-db.p.rapidapi.com/list.php?c=list",
+        method: "GET",
+        headers: {
+          "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
+          "x-rapidapi-key": "800dedb80dmsh5623edb79c19968p10818fjsnd60acfd537d3",
+        }
+      }).then(function (response) {
+        for (i = 0; i <= 32; i++) {
+          $('.categoriesOption').append("<div>" + response.drinks[i].strCategory + "</div>" + "<br>");
+        }
+      });
+    };
+    // End Glasses List
 
-  // login button / modal function
+    //corona_cocktail calls
+    function getAllReviews() {
+      $.ajax({
+        async: true,
+        url: "https://backend-project-2.herokuapp.com/reviews",
+        method:"GET"
+      }).then((res) =>{
+        console.log(res);
+      });
+    };
 
-  $(document).on("click",".login-modal",function(){
-    $("#myModal").modal('show');
-    return false
-  });
-  // end login button / modal function
+    function getRatingbyId(reviews_id) {
+      $.ajax({
+        async: true,
+        url: `https://backend-project-2.herokuapp.com/rating/${reviews_id}`,
+        method:"GET"
+      }).then((res) =>{
+        console.log(res);
+      });
+    };
+
+    function getReviewbyId(reviews_id) {
+      $.ajax({
+        async: true,
+        url: `https://backend-project-2.herokuapp.com/review/${reviews_id}`,
+        method:"GET"
+      }).then((res) =>{
+        console.log(res);
+      });
+    };
+
+    function getReviewsByCocktail(reviews_id) {
+      $.ajax({
+        async: true,
+        url: `https://backend-project-2.herokuapp.com/reviews/cocktail/${reviews_id}`,
+        method:"GET"
+      }).then((res) =>{
+        console.log(res);
+      });
+    };
+
+    //the post and patch have not been tested out nor am I sure if I wrote it correctly. --Juan
+    function postNewReview() {
+      $.ajax({
+        async: true,
+        url: `https://backend-project-2.herokuapp.com/reviews`,
+        method:"POST"
+      }).then((res) =>{
+        console.log(res);
+      });
+    };
+    
+    function patchReviewById(reviews_id) {
+      $.ajax({
+        async: true,
+        url: `https://backend-project-2.herokuapp.com/reviews/${reviews_id}`,
+        method:"PATCH"
+      }).then((res) =>{
+        console.log(res);
+      });
+    };
+    
+
+    
+    // Search bar / find drink by name
+
+
+
+    // login button / modal function
+
+    $(document).on("click", ".login-modal", function () {
+      $("#myModal").modal('show');
+      return false
+    });
+    // end login button / modal function
+
+    // sign up button start
+    $(document).on("click", ".signUp", function () {
+      $("#signUpModal").modal('show');
+      $('myModal').modal('hide')
+      return false
+    });
+    //end sign up button
+
+    // Search Button
+    $(document).on("click", ".go", function () {
+      console.log("searched")
+    });
+
+
   
-// sign up button start
-$(document).on("click",".signUp",function(){
-  $("#signUpModal").modal('show');
-  $('myModal').modal('hide')
-  return false
-})
-//end sign up button
-
-// Search Button
-$(document).on("click", ".go" ,function(){
-  console.log("searched")
-})
-}
-  }
-    }
-     });
+});
